fc498fb812295c54392ee6790320a5a8dafb90df
sudipz <outsudip@outlook.com>
2026-06-05 02:40:36 +0530

    Initial commit

---
+++ IRCFC Website/js/counter.js
@@ -0,0 +1,34 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+counter.innerText = '0';
+
+const updateCounter = () => {
+
+const target = +counter.getAttribute('data-target');
+
+const c = +counter.innerText;
+
+const increment = target / 100;
+
+if(c < target){
+
+counter.innerText =
+`${Math.ceil(c + increment)}`;
+
+setTimeout(updateCounter,20);
+
+}
+
+else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
+});
